02e8e01cdebaddd90b46ad570f2b82999a214e3d
EstebanAaron <lr96aron@gmail.com>
2025-05-15 01:16:47 +0100

    fix: Añade la importación de Storage en PublicationController

--- app/Http/Controllers/PublicationController.php
+++ app/Http/Controllers/PublicationController.php
@@ -7,6 +7,7 @@ use App\Models\Hashtag;
 use App\Models\Mention;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
 
 class PublicationController extends Controller
